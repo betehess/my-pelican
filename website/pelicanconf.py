21aa9bc8a071a91b98125cfb8a66b515e69b5f64
Alexandre Bertails <bertails@gmail.com>
2014-08-14 21:51:18 -0400

    starting hacking on theme betehess

--- website/pelicanconf.py
+++ website/pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Alexandre Bertails <alexandre@bertails.org>'
+AUTHOR = u'betehess'
 SITENAME = u"Alexandre Bertails' blog"
 SITEURL = ''
 
@@ -32,4 +32,45 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-MD_EXTENSIONS = ['codehilite','extra','headerid']
+MD_EXTENSIONS = ['codehilite(css_class=codehilite code)','extra','headerid']
+
+DESCRIPTION = 'My blog and stuff ...@@@@'
+
+# bootstrap built-texts BT3-Flat Responsive-Pelican
+THEME = 'themes/betehess'
+
+GITHUB_URL = "https://github.com/betehess"
+
+TWITTER_USERNAME = 'bertails'
+
+SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
+          ('lastfm', 'http://lastfm.com/user/akounet'),
+          ('github', 'http://github.com/ametaireau'),)
+
+#SITEURL = 'http://bertails.org/blog'
+
+#FILENAME_METADATA = '(?P<slug>.*)'  # use markdown file name as the slug meta
+#USE_FOLDER_AS_CATEGORY = True       # use folder name as posts' category
+
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_LANG_URL = '{date:%Y}/{date:%m}/{slug}-{lang}'
+ARTICLE_LANG_SAVE_AS = '{date:%Y}/{date:%m}/{slug}-{lang}.html'
+
+DRAFT_URL = 'drafts/{slug}.html'
+DRAFT_SAVE_AS = 'drafts/{slug}.html'
+DRAFT_LANG_URL = 'drafts/{slug}-{lang}.html'
+DRAFT_LANG_SAVE_AS = 'drafts/{slug}-{lang}.html'
+
+CATEGORY_URL = ''
+CATEGORY_SAVE_AS = ''
+
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}.html'
+
+AUTHOR_URL = ''
+AUTHOR_SAVE_AS = ''
+
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
