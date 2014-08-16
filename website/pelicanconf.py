2fca5558b30fdb87f3b7131c1aa53c72df384ddc
Alexandre Bertails <bertails@gmail.com>
2014-08-16 18:30:21 -0400

    using my-theme!

--- website/pelicanconf.py
+++ website/pelicanconf.py
@@ -3,29 +3,27 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'betehess'
-SITENAME = u"Alexandre Bertails' blog"
-SITEURL = ''
+SITENAME = u"Alexandre Bertails"
+#SITEURL = 'http://bertails.org'
 
 PATH = 'content'
 
 TIMEZONE = 'EST'
 
 DEFAULT_LANG = u'en'
+DELETE_OUTPUT_DIRECTORY = True
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+#feeds
+FEED_ALL_ATOM = 'feeds/all.xml'
+FEED_ALL_RSS = 'feeds/all.rss'
+CATEGORY_FEED_ATOM = 'feeds/%s.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss'
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 TRANSLATION_FEED_ATOM = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+TRANSLATION_FEED_RSS = None
+FEED_ATOM = None
+FEED_RSS = None
 
 DEFAULT_PAGINATION = 10
 
@@ -34,29 +32,14 @@ DEFAULT_PAGINATION = 10
 
 MD_EXTENSIONS = ['codehilite(css_class=codehilite code)','extra','headerid']
 
-DESCRIPTION = 'My blog and stuff ...@@@@'
-
 # bootstrap built-texts BT3-Flat Responsive-Pelican
-THEME = 'themes/betehess'
+THEME = 'my-theme'
+THEME_STATIC_DIR = ''
 
-GITHUB_URL = "https://github.com/betehess"
-
-TWITTER_USERNAME = 'bertails'
-
-SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
-          ('lastfm', 'http://lastfm.com/user/akounet'),
-          ('github', 'http://github.com/ametaireau'),)
-
-#SITEURL = 'http://bertails.org/blog'
-
-#FILENAME_METADATA = '(?P<slug>.*)'  # use markdown file name as the slug meta
-#USE_FOLDER_AS_CATEGORY = True       # use folder name as posts' category
-
-
-ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}'
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
-ARTICLE_LANG_URL = '{date:%Y}/{date:%m}/{slug}-{lang}'
-ARTICLE_LANG_SAVE_AS = '{date:%Y}/{date:%m}/{slug}-{lang}.html'
+ARTICLE_URL = '{slug}'
+ARTICLE_SAVE_AS = '{slug}.html'
+ARTICLE_LANG_URL = '{slug}-{lang}'
+ARTICLE_LANG_SAVE_AS = '{slug}-{lang}.html'
 
 DRAFT_URL = 'drafts/{slug}.html'
 DRAFT_SAVE_AS = 'drafts/{slug}.html'
@@ -66,11 +49,16 @@ DRAFT_LANG_SAVE_AS = 'drafts/{slug}-{lang}.html'
 CATEGORY_URL = ''
 CATEGORY_SAVE_AS = ''
 
-TAG_URL = 'tag/{slug}'
-TAG_SAVE_AS = 'tag/{slug}.html'
+TAG_URL = None
+TAG_SAVE_AS = None
 
 AUTHOR_URL = ''
 AUTHOR_SAVE_AS = ''
 
-YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
-MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
+ARCHIVES_SAVE_AS = None
+AUTHORS_SAVE_AS = None
+CATEGORIES_SAVE_AS = None
+TAGS_SAVE_AS = None
+
+#YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+#MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
